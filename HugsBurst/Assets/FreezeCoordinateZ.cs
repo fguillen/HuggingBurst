f02a94bebbd1f2599c8619ce7c8d1ddcbb1a3d27
Fernando Guillen <fguillen.mail@gmail.com>
2020-11-17 21:01:38 +0100

    Camera confiner

---
+++ HugsBurst/Assets/FreezeCoordinateZ.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeCoordinateZ : MonoBehaviour
+{
+    float originalZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalZ = transform.position.z;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentPosition = transform.position;
+        currentPosition.z = originalZ;
+        transform.position = currentPosition;
+    }
+}
